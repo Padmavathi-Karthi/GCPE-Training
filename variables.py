6910219b22de99632cb4f545b22ee9538bc2b635
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-08-27 21:54:47 +0100

    group exercise added

---
+++ variables.py
@@ -0,0 +1,19 @@
+# Group exercise in variables:
+
+full_name = input("What is your full name?: ")
+
+job_name = input("What is your Cohort name?: ")
+
+adjective_cohort1 = input("How you are feeling about your cohort?: ")
+
+adjective_cohort2 = input("What would you say about your instructor teaching?: ")
+
+food_preference1 = input("What is your favourite food?: ")
+
+food_preference2 = input("What is your second favourite food?: ")
+
+feeling_1 = input("How do you feel about  the course?: ")
+
+print("/n")
+
+print(full_name  + " " +  " started their first Generation course today." + " " " They are training as a" " " + job_name + " " "They found their cohort to be very" " " + adjective_cohort1 + " " "but their teacher was, at least," +  adjective_cohort2 + "." + "For lunch they have" " " + food_preference1 + " " "and" " " + food_preference2 + " " "while reviewing their notes." + "They feel" " " + feeling_1  + " " "but are determined to complete the course." )
