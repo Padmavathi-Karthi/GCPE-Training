961badec88321d4c799a16bfdc7d2a8551c167b1
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-08-28 21:34:58 +0100

    datatypes added

---
+++ Datatypes.py
@@ -0,0 +1,20 @@
+# Integers
+integer_1 = 10
+integer_2 = 0
+
+# Floating point number (Float)
+float_1 = 12.3
+float_2 = -5.2
+
+# String
+string_1 = "this is a sentence."
+string_2 = "and this is another."
+
+# Boolean
+boolean_1 = True
+boolean_2 = False
+
+# type()
+print(type(integer_1))  # method 1
+integer = type(10)
+print(integer)
