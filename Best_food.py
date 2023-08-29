287da975981fa6b67ed9182d458be9c3f11161e0
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-08-29 11:16:51 +0100

    another if statement added

--- Best_food.py
+++ Best_food.py
@@ -4,5 +4,13 @@ if food == "rice":
     print("Yes, rice is the best.")
 elif food == "apple":
     print("Apples aren't my cup of rice.")
+elif food == "chicken":
+    print("Chickens are good in proteins!")
 else:
     print("Never heard of it!")
+new_food = input("What is your second best food?: ")
+if new_food == "rice":
+    print("Yes this food is best")
+else:
+    print("Not in the list!")
+
