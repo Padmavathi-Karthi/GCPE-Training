a22e77a16880af781b8b2aa4e7b3c023073aa93a
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-08-29 11:26:31 +0100

    another statement added

---
+++ Best_names (29.08.23).py
@@ -0,0 +1,11 @@
+# your first for loop: can you guess what it does?
+
+names_i_like = ['Thomson', 'Thompson', 'Haddock', 'Snowy']
+
+best_countries = ['India', 'United Kingdom', 'United States', 'Singapore']
+
+for name in names_i_like:
+    print(f"I like this name: {name}")
+
+for country in best_countries:
+    print(f"Among other countries, I like '{country}' the most! ")
