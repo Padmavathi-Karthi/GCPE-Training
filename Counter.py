a0b994c3a57389bd46a7e03c026c1c619c9cbdda
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-08-29 10:19:05 +0100

    break modified

--- Counter.py
+++ Counter.py
@@ -1,10 +1,14 @@
 # your first while loop: can you guess what it does?
 
-limit = 12
+limit = 120
 
 counter = 1
 while counter < limit:
-    print(counter)
+    if counter > 10:
+        break
+    else:
+        print(counter)
     counter = counter + 1
     # print("Finished")   # it will print in each and every lines.
+    
 print("Finished")   # it will print only if the condition is not satisfied
