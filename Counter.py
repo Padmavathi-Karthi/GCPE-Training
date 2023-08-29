4d98d0bbf320cf39e265f7882c8915a3979ddf47
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-08-29 10:09:34 +0100

    print statement added

--- Counter.py
+++ Counter.py
@@ -6,3 +6,5 @@ counter = 1
 while counter < limit:
     print(counter)
     counter = counter + 1
+    # print("Finished")   # it will print in each and every lines.
+print("Finished")   # it will print only if the condition is not satisfied
