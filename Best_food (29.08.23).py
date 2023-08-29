42916cbfefedaa66a636300e77470b66d385ef53
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-08-29 13:42:54 +0100

    example exercise added

--- Best_food (29.08.23).py
+++ Best_food (29.08.23).py
@@ -1,8 +1,9 @@
+
 food = input("What's the best food?: ")
 
 if food == "rice":
     print("Yes, rice is the best.")
-elif food == "apple":
+elif food.lower() == "apple":    # lower(), it will convert the user's input to lower case
     print("Apples aren't my cup of rice.")
 elif food == "chicken":
     print("Chickens are good in proteins!")
@@ -14,4 +15,36 @@ if new_food == "rice":
     print("Yes this food is best")
 else:
     print("Not in the list!")
+
+print("/n")
+
+# Example 1:
+
+answer = input("What are your favourite trainers? type 'j' for Jordan's, 'd' for Dunks or 'a' for Ant! ")
+while answer != "j" and answer != "d" and answer != "a":
+    answer = input("That is not a valid response, type 'j', 'd' or 'a'!: ")
+if answer == "j":
+    print("You favourite trainers are Jordan's")
+elif answer == "d":
+    print("You favourite trainers are Dunks")
+elif answer == "a":
+    print("You favourite trainers are Ant")
+
+
+print("/n")
+
+# Example 2:
+
+limit = 5
+counter = 1
+food = input("What's the best food?: ")
+while counter < limit:
+    if food != "chicken" and food != "rice":
+        print("This food is not in the list!")
+    #elif food == "chicken" or food == "rice":
+        #print("This food is in the list!")
+    else:
+        print("This food is in the list!")
+
+    counter = counter + 1
 
