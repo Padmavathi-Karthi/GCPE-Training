60bc2e1e41cc1c1e68a6035a1a6fba1e0864c2a1
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-09-06 17:31:04 +0100

    3 numbers added

---
+++ calculator 3 numbers.py
@@ -0,0 +1,10 @@
+
+def add(num1, num2, num3):
+  result = int(num1) + int(num2) + int(num3)
+  print(result)
+
+num1 = input('Enter the first number: ')
+num2 = input('Enter the second number: ')
+num3 = input('Enter the third number: ')
+
+add(num1, num2, num3)
